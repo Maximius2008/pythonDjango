135b35b99e53e2fae7448f0f541c41b8519a619e
Maximius2008 <krutoikrasavec@gmail.com>
2023-07-11 22:23:11 +0300

    Джанго заглушка

---
+++ app_lesson_4/lesson_4/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def zaglushka(request):
+    return HttpResponse('Длмашка по 4 занятию! (у меня пропала папка с урока, так что пришлось делать всё заново)))')
